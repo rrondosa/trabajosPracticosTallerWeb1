1de8946ed387b28e6377f909f0f45c6a9b98e886
Martin Mendez <martinhmsuni@gmail.com>
2018-04-25 17:12:32 -0300

    P1 TestFarmaciasTurnoMartes
    Se agrega el test de Farmacias con turno Martes.
    (No funciona)

---
+++ proyecto-limpio-spring/src/test/java/ar/edu/unlam/tallerweb1/persistencia/FarmarciasTunoDiaMartes.java
@@ -0,0 +1,45 @@
+package ar.edu.unlam.tallerweb1.persistencia;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import ar.edu.unlam.tallerweb1.SpringTest;
+import ar.edu.unlam.tallerweb1.modelo.Farmacia;
+
+public class FarmarciasTunoDiaMartes extends SpringTest{
+
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void TestFarmarciasTunoDiaMartes() {
+
+		Session sessionTest = getSession();
+		
+		Farmacia farmacia1 = new Farmacia();
+		Farmacia farmacia2 = new Farmacia();
+		Farmacia farmacia3 = new Farmacia();
+		Farmacia farmacia4 = new Farmacia();
+		
+		farmacia1.setDiaDeTurno("Martes");
+		farmacia2.setDiaDeTurno("Martes");
+		farmacia3.setDiaDeTurno("Jueves");
+		farmacia4.setDiaDeTurno("Miercoles");
+		
+		sessionTest.save(farmacia1);
+		sessionTest.save(farmacia2);
+		sessionTest.save(farmacia3);
+		sessionTest.save(farmacia4);
+		
+		List<Farmacia> ListaDeFarmaciasTurnoMartes = getSession().createCriteria(Farmacia.class).add(Restrictions.eqOrIsNull("diaDeTurno", "Martes")).list();
+		assertThat(ListaDeFarmaciasTurnoMartes.size()).isEqualTo(2);
+				
+	}
+}
